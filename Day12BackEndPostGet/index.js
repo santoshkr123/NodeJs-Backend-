0d4c3483e91c690febfa9ad48318ed8f50df8692
Santosh Kumar <santosh.rash44@gmail.com>
2024-09-13 22:05:19 +0530

    adedd

--- Day12BackEndPostGet/index.js
+++ Day12BackEndPostGet/index.js
@@ -4,6 +4,9 @@ const server = http.createServer((req,res)=>{
     res.writeHead(200,{
         "Access-Control-Allow-Origin" : "http://127.0.0.1:5000"
     })
+    const method = req.body 
+    if(method == 'POST')
+    {
     let data =''
    req.on('data',(chunk)=>{
     data = data +chunk.toString()
@@ -11,11 +14,19 @@ const server = http.createServer((req,res)=>{
    })
    req.on('end',()=>{
     const obj =JSON.parse(data)
-    fs.writeFileSync(obj.filename,obj.content)
+    fs.writeFileSync('./stoarge/'+obj.filename,obj.content)
     res.write("success")
     res.end()
    })
+}
 
+else if(method =='GET')
+{
+   const file= fs.readdirSync('./storage')
+   res.write(JSON.stringify(files))
+   res.end()
+   
+}
     
 })
 server.listen(8080)
